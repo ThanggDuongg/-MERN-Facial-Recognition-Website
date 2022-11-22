6685a95ca2c4c911cb1209d58d6e91d97499d91f
Thang Duong <thangduong@futurify.vn>
2022-11-22 22:25:32 +0700

    feat: CPM-14: refactor ui, add page service for prj

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import BackToTop from './components/BackToTop/index';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ConfirmEmailPage from './pages/auth/ConfirmEmailPage';
+import FaceDetectImagePage from './pages/service/FaceDetectImagePage';
+import FaceDetectRealtimePage from './pages/service/FaceDetectRealtimePage';
+import FaceDetectImageMultiPeoplePage from './pages/service/FaceDetectImageMultiPeople';
+import TensorflowPage from './pages/service/TensorflowPage';
+import { MultiFaceDetectionImagePage } from './pages/service/MultiFaceDetectionImagePage';
+import { RegisterFacePage } from './pages/auth/RegisterFacePage';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <BackToTop key={250} />
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+
+          <Route path="/" element={<Navigate to='/home' replace />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register-face" element={<RegisterFacePage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+          <Route path="/face-detect-image" element={<FaceDetectImagePage />} />
+          <Route path="/face-detect-realtime" element={<FaceDetectRealtimePage />} />
+          <Route path="/face-detect-image-multipeople" element={<FaceDetectImageMultiPeoplePage />} />
+          <Route path="/tensorflow" element={<TensorflowPage />} />
+          <Route path='/multiface-detection-image' element={<MultiFaceDetectionImagePage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
